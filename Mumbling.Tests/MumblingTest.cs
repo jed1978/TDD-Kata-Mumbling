f8fc273b7f91aa45758405ae0a26db9cb36508b0
Jed Lin <jed1978@gmail.com>
2018-05-31 00:17:47 +0800

    Implement test case Test_Entered_xb_ShouldReturn_X_Bb

--- Mumbling.Tests/MumblingTest.cs
+++ Mumbling.Tests/MumblingTest.cs
@@ -14,6 +14,12 @@ namespace Mumbling.Tests
             actual.ShouldBeEqualTo("X");
         }
 
-        
+        [Test]
+        public void Test_Entered_xb_ShouldBe_X_Bb()
+        {
+            var accuml = new Accuml();
+            var actual = accuml.Accum("xb");
+            actual.ShouldBeEqualTo("X-Bb");
+        }
     }
 }
--- Mumbling/Accuml.cs
+++ Mumbling/Accuml.cs
@@ -1,9 +1,23 @@
-﻿namespace Mumbling
+﻿using System;
+using System.Linq;
+
+namespace Mumbling
 {
     public class Accuml
     {
         public string Accum(string s)
         {
+            if (s.Length > 1)
+            {
+                var accumCharArray = s.ToCharArray();
+                var accumStringArray = new[]
+                {
+                    accumCharArray[0].ToString().ToUpper(),
+                    accumCharArray[1].ToString().ToUpper() + accumCharArray[1]
+                };
+                return String.Join("-", accumStringArray);
+            }
+
             return s.ToUpper();
         }
     }
