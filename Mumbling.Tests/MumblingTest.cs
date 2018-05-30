222a1be326d3a200391ff09ab48c1ee16e357fc3
Jed Lin <jed1978@gmail.com>
2018-05-31 00:21:55 +0800

    Refactor test case, introduce variables

--- Mumbling.Tests/MumblingTest.cs
+++ Mumbling.Tests/MumblingTest.cs
@@ -6,20 +6,24 @@ namespace Mumbling.Tests
     [TestFixture]
     public class MumblingTest
     {
+        private Accuml _accuml = new Accuml();
+
         [Test]
         public void Test_Entered_x_ShouldReturn_X()
         {
-            var accuml = new Accuml();
-            var actual = accuml.Accum("x");
-            actual.ShouldBeEqualTo("X");
+            var actual = _accuml.Accum("x");
+            var expected = "X";
+            actual.ShouldBeEqualTo(expected);
         }
 
         [Test]
         public void Test_Entered_xb_ShouldBe_X_Bb()
         {
-            var accuml = new Accuml();
-            var actual = accuml.Accum("xb");
-            actual.ShouldBeEqualTo("X-Bb");
+            var actual = _accuml.Accum("xb");
+            var expected = "X-Bb";
+            actual.ShouldBeEqualTo(expected);
         }
+
+        
     }
 }
